782d294a03d2c3ff085c91c014d321da9e587070
slowfever@blueglowad.com <slowfever@blueglowad.com>
2024-01-11 21:09:07 +0800

    西瓜播放器组件 - 视频来源于网络，小姐姐API。

---
+++ watermelonplayer/js/index.js
@@ -0,0 +1,60 @@
+let next = document.getElementById('next')
+let continuous = document.getElementById('continuous')
+let State = continuous.querySelector('span')
+
+
+window.onload = () => {
+    function getRandomVideos(){
+        let randomInt = Math.floor(Math.random() * (99999 - 1 + 1)) + 1;
+        let url = `https://v.nrzj.vip/video.php?_t=${randomInt}`
+        let setVideoURL = (url) => url
+        return setVideoURL(url)
+    }
+    let randomVideoURL = getRandomVideos()
+
+    const config = {
+        "id": "mse",
+        "playsinline": true,
+        /*"poster": "//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg",*/
+        "plugins": [],
+        "url": randomVideoURL,
+        "autoplay": true,
+        "width": "500",
+        "fluid": true
+    }
+
+    let player = new Player(config)
+    console.log(player)
+
+    next.addEventListener('click',() => {
+        player.playNext({
+            "url": getRandomVideos(),
+        })
+    })
+
+    let state = false
+    continuous.addEventListener('click',() => {
+        State ? (State.innerText === '关' ? (state = true, State.innerText = '开') : (State.innerText = '关', state = false)) : null;
+        if (state) {
+            // 监听 ended 事件
+            player.on('ended', () => {
+                player.playNext({
+                    "url": getRandomVideos(),
+                })
+            })
+            State.classList.add('textColourRed')
+            console.log("监听 ended 事件")
+        } else {
+            player.off('ended'); // 取消监听 ended 事件
+            State.classList.remove('textColourRed')
+            console.log("取消监听 ended 事件")
+        }
+    })
+
+    /*
+            player.on('error', (error) => {
+                console.log(error)
+            })
+    */
+
+}
