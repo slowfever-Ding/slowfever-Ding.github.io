8edf6a1bcc8d3e1f147e60b1acbaaae87618660e
slowfever@blueglowad.com <slowfever@blueglowad.com>
2024-01-31 21:26:07 +0800

    更改使用 ES6 class 类 面向对象方式函数

--- baidusearchpromptwords/js/index.js
+++ baidusearchpromptwords/js/index.js
@@ -3,7 +3,7 @@ let list = document.getElementById('list');
 let next = document.getElementById('next');
 let milf = document.getElementById('milf');
 
-
+// 搜索提示
 mySearch.oninput = function (event) {
     /*console.log(event.target.value)*/
 
@@ -49,57 +49,75 @@ function test(obj) {
 
 }
 
-window.onload = function () {
 
-    const apiUrl = 'https://api.waifu.im/search';  // Replace with the actual API endpoint URL
-    let params = {
-        included_tags: 'maid',
-        height: '>=2000'
-    };
-    let queryParams = new URLSearchParams(params);
-    let requestUrl = `${apiUrl}?${queryParams}`;
+// 右下角菜单
+let ua = navigator.userAgent;
+/Safari|iPhone/i.test(ua) && 0 == /chrome/i.test(ua) && $("#aside-nav").addClass("no-filter")
+let drags = {down: !1, x: 0, y: 0, winWid: 0, winHei: 0, clientX: 0, clientY: 0},
+    asideNav = $("#aside-nav")[0],
+    getCss = function (a, e) {
+        return a.currentStyle ? a.currentStyle[e] : document.defaultView.getComputedStyle(a, !1)[e]
+    }
+$("#aside-nav").on("mousedown", function (a) {
+    drags.down = !0,
+        drags.clientX = a.clientX,
+        drags.clientY = a.clientY,
+        drags.x = getCss(this, "right"),
+        drags.y = getCss(this, "top"),
+        drags.winHei = $(window).height(),
+        drags.winWid = $(window).width(),
+        $(document).on("mousemove", function (a) {
+            if (drags.winWid > 640 && (a.clientX < 120 || a.clientX > drags.winWid - 50))//50px
+                return !1 /*,console.log(!1)*/;
+            if (a.clientY < 180 || a.clientY > drags.winHei - 120)//导航高度
+                return !1;
+            let e = a.clientX - drags.clientX,
+                t = a.clientY - drags.clientY;
+            asideNav.style.top = parseInt(drags.y) + t + "px";
+            asideNav.style.right = parseInt(drags.x) - e + "px";
+        })
+}).on("mouseup", function () {
+    drags.down = !1, $(document).off("mousemove")
+})
 
-    // 下一张
-    next.onclick = function () {
-        getBackgroundImage()
+
+
+class BackgroundImageFunction{
+    constructor() {
+        this.bgImg = document.body
+        this.arr = ['maid', 'milf'] // 定义一个数组 来存 tags 参数
+        this.arrItem = JSON.stringify(this.arr[0]) // 默认数组下标0第一项 ‘maid’
+        // 去除属性名两侧的双引号，将数组下标0第一项 ‘maid’存入localStorage
+        localStorage.setItem('tags', this.arrItem.replace(/"/g, ''))
+        this.apiUrl = 'https://api.waifu.im/search' // Replace with the actual API endpoint URL
+        this.queryParams = new URLSearchParams(this.set_included_tag())
+        /*console.log(JSON.stringify(arr[0]))*/
+        // 调用初始化方法
+        let promise = this.init();
+        console.log(promise)
     }
 
-    // milf
-    milf.onclick = function () {
-        params = {
-            included_tags: 'milf',
-        }
-        let queryParams = new URLSearchParams(params);
-        let requestUrl = `${apiUrl}?${queryParams}`;
-        let typeConversion = () => {
-            fetch(requestUrl)
-                .then(response => {
-                    if (response.ok) {
-                        return response.json();
-                    } else {
-                        return Promise.reject({
-                            status: response.status,
-                            statusText: response.statusText
-                        })
-                    }
-                })
-                .then(data => {
-                    let test = data.images.map((value) => {
-                        return value
-                    })
-                    console.log(test[0].url)
-                    document.body.style.backgroundImage = `url(${test[0].url})`;
-                })
-                .catch(error => {
-                    console.log(error)
-                });
-        }
-        typeConversion()
+    // 初始化
+    async init(){
+        this.set_included_tag()
+        console.log(this.set_included_tag())
+        await this.setBackgroundImage()
     }
 
+    // 设置当前背景图片类型
+    set_included_tag(){
+        // 从 localStorage 中取当前存入的值
+        this.tags = localStorage.getItem('tags')
+        return {
+            included_tags: this.tags,
+            height: ">=2000"
+        };
+    }
 
-    function getBackgroundImage() {
-        fetch(requestUrl)
+    // 请求获取背景图片
+    async getBackgroundImage(){
+        this.requestUrl = `${this.apiUrl}?${this.queryParams}`
+        return await fetch(this.requestUrl)
             .then(response => {
                 if (response.ok) {
                     return response.json();
@@ -111,43 +129,37 @@ window.onload = function () {
                 }
             })
             .then(data => {
-                let test = data.images.map((value) => {
-                    return value
-                })
-                console.log(test[0].url)
-                document.body.style.backgroundImage = `url(${test[0].url})`;
+                return data
             })
             .catch(error => {
                 console.log(error)
-            });
+            })
     }
 
-    getBackgroundImage()
 
+    // 设置背景图片
+    async setBackgroundImage(){
+        // 获取背景图片
+        this.data = await this.getBackgroundImage()
+        this.result = this.data.images.map((item) => {
+            return item
+        })
+        console.log(this.result[0].url)
+        this.bgImg.style.backgroundImage = `url(${this.result[0].url})`
+    }
 }
 
-let ua=navigator.userAgent; /Safari|iPhone/i.test(ua)&&0==/chrome/i.test(ua)&&$("#aside-nav").addClass("no-filter");
-let drags={down:!1,x:0,y:0,winWid:0,winHei:0,clientX:0,clientY:0},
-    asideNav=$("#aside-nav")[0],
-    getCss=function(a,e){return a.currentStyle?a.currentStyle[e]:document.defaultView.getComputedStyle(a,!1)[e]};
-$("#aside-nav").on("mousedown",function(a){
-    drags.down=!0,
-        drags.clientX=a.clientX,
-        drags.clientY=a.clientY,
-        drags.x=getCss(this,"right"),
-        drags.y=getCss(this,"top"),
-        drags.winHei=$(window).height(),
-        drags.winWid=$(window).width(),
-        $(document).on("mousemove",function(a){
-            if(drags.winWid>640&&(a.clientX<120||a.clientX>drags.winWid-50))//50px
-                return !1 /*,console.log(!1)*/;
-            if(a.clientY<180||a.clientY>drags.winHei-120)//导航高度
-                return!1;
-            let e=a.clientX-drags.clientX,
-                t=a.clientY-drags.clientY;
-            asideNav.style.top=parseInt(drags.y)+t+"px";
-            asideNav.style.right=parseInt(drags.x)-e+"px";
-        })
-}).on("mouseup",function(){drags.down=!1,$(document).off("mousemove")});
+let background_image_function = new BackgroundImageFunction()
 
+// 下一张
+next.onclick = async function () {
+    await background_image_function.init()
+}
 
+// milf
+milf.onclick = async function () {
+    // 将数组下标1第二项 ‘milf’存入localStorage - 覆盖默认值
+    background_image_function.arrItem = JSON.stringify(background_image_function.arr[1])
+    localStorage.setItem('tags', background_image_function.arrItem.replace(/"/g, '')) // 去除属性名两侧的双引号
+    await background_image_function.init()
+}
