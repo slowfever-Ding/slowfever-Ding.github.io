f1956ee21bb695989db0afd93499a7e8c47af394
slowfever@blueglowad.com <slowfever@blueglowad.com>
2024-04-19 21:22:49 +0800

    initial commit

---
+++ appreciationofaccordion/js/data.js
@@ -0,0 +1,32 @@
+let data = [
+    {
+        'img': '../images/yisetaozi.jpg',
+        'title': 'TOP-001',
+        'description': '一色桃子（日文：一色桃子，1977年3月3日—），日本AV女優。 所屬事務所是EIGHTMAN。'
+    },
+    {
+        'img': '../images/zuoshanai.jpg',
+        'title': 'TOP-002',
+        'description':'佐山爱（日文：佐山愛，1989年1月8日－）是日本的AV女优 。出身于日本东京都，所属于“T-POWERS”事务所。于2007年出道于AV成人电影界，拍摄过许多AV作品。除了拍摄AV之外，佐山爱自称恋爱经验超过50次，还推出追求女性的教学影片在网路上贩售。'
+    },
+    {
+        'img': '../images/jigenyouliai.jpg',
+        'title': 'TOP-003',
+        'description':'吉根柚莉愛（日文：吉根 ゆりあ／よしね ゆりあ Yoshine Yuria，1994年5月26日—），日本主持人、AV女優，曾經是脫衣舞孃。 所屬「ビーダッシュプロモーション」事務所。'
+    },
+    {
+        'img': '../images/yizhiliangzi.jpg',
+        'title': 'TOP-004',
+        'description':'伊織涼子（日文：いおり りょうこ、1969年9月23日 - ）は、日本のAV女優。C-more ENTERTAINMENT所属。'
+    },
+    {
+        'img': '../images/hengshanmeiling.jpg',
+        'title': 'TOP-005',
+        'description':'横山美玲（日文：橫山みれい、英语：Mirei Yokoyama、1983年11月23日—）是日本的AV女优。2009年出道拍摄av电影。作品主要以熟女、痴女、若妻系列为主。'
+    },
+    {
+        'img': '../images/beichuanlizi.jpg',
+        'title': 'TOP-006',
+        'description':'北川礼子（日文：北川礼子，1974年7月18日），日本AV女优。所属事务所是ジュエルズプロダクション。'
+    },
+]
