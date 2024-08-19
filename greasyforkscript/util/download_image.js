567144c40d6df9e189d47bc657ac70fe90f2767d
slowfever <slowfever.ding@gmail.com>
2024-08-19 21:14:08 +0800

    Initial commit

---
+++ greasyforkscript/util/download_image.js
@@ -0,0 +1,55 @@
+// ==UserScript==
+// @name         Image Downloader Script
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  下载网页上的图片并保存到本地
+// @author       slowFever
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // 获取图片节点的函数
+    function getDocumentImgNode(nodes) {
+        let imgNodes = [];
+        nodes.forEach(node => {
+            const src = node.getAttribute('src');
+            if (src && !imgNodes.includes(src)) {
+                imgNodes.push(src);
+            }
+        });
+        return imgNodes;
+    }
+
+    // 下载单张图片的函数
+    function downloadImage(url, name) {
+        fetch(url)
+            .then(response => response.blob())
+            .then(blob => {
+                const link = document.createElement('a');
+                link.href = URL.createObjectURL(blob);
+                link.download = name;
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+            })
+            .catch(console.error);
+    }
+
+    // 依次下载图片的函数
+    function downloadImagesSequentially(imgUrls) {
+        imgUrls.forEach((url, index) => {
+            const fileName = `image-${index + 1}.${url.split('.').pop().split('?')[0]}`;
+            downloadImage(url, fileName);
+        });
+    }
+
+    // 将函数暴露到全局，以便在控制台调用
+    window.downloadImagesSequentially = downloadImagesSequentially;
+    window.getDocumentImgNode = getDocumentImgNode;
+
+    // 提示用户可以通过控制台调用函数
+    console.log('已加载图像下载器脚本。使用控制台调用带有所需图像节点的方法 downloadImagesSequentially(getDocumentImgNode(img节点))');
+})();
